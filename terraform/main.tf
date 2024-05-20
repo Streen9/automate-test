f872eb55fff47feaed6df30283da661ed3736f9b
Streen9 <gkvp70@gmail.com>
2024-05-20 22:34:07 +0530

    adding permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,22 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.this[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.this[0].arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_website_configuration" "example" {
   count  = length(aws_s3_bucket.this)
   bucket = aws_s3_bucket.this[0].id
