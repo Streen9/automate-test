0947c45eedbf4506e6e1ff20734aa5010d2b1436
Streen9 <gkvp70@gmail.com>
2024-05-20 20:02:48 +0530

    cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,88 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "tf-bucket-automate-test"
-  acl    = "private"
+  bucket = var.bucket_name
+  acl    = "public-read"
 
   control_object_ownership = true
-  object_ownership         = "ObjectWriter"
+  object_ownership         = "BucketOwnerPreferred"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 
   versioning = {
     enabled = true
   }
+  website = {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+
+module "cloudfront" {
+  source  = "terraform-aws-modules/cloudfront/aws"
+  version = "~> 3.2.0"
+
+  origin = [{
+    domain_name = module.s3_bucket.s3_bucket_bucket_regional_domain_name
+    origin_id   = var.bucket_name
+  }]
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "cloudfront with s3 ${var.bucket_name}"
+  default_root_object = var.cloudfront_default_root_object
+  price_class         = var.cloudfront_price_class
+
+  default_cache_behavior = {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = var.bucket_name
+
+    forward_values = {
+      query_string = false
+
+      cookies = {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  viewer_certificate = {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "s3_bucket_domain_name" {
+  value = module.s3_bucket.s3_bucket_bucket_regional_domain_name
+}
+
+output "cloudfront_domain_name" {
+  value = module.cloudfront.cloudfront_domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = module.cloudfront.cloudfront_distribution_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,36 @@ variable "aws_region" {
   type        = string
   description = "AWS region in which the users and groups are managed with iam identity centre"
 }
+
+variable "bucket_name" {
+  default = "tf-bucket-automate-test"
+  type = string
+}
+
+variable "created_by" {
+  default = "kalivaraprasad gonapa" 
+  type = string
+}
+
+variable "object_ownership" {
+  default = "BucketOwnerPreferred"
+  type = string
+}
+
+variable "cloudfront_default_root_object" {
+  description = "The default root object for the CloudFront distribution"
+  type        = string
+  default     = "index.html"
+}
+
+variable "cloudfront_origin_path" {
+  description = "The CloudFront origin path"
+  type        = string
+  default     = ""
+}
+
+variable "cloudfront_price_class" {
+  description = "The price class for the CloudFront distribution"
+  type        = string
+  default     = "PriceClass_All"
+}
