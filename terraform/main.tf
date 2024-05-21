e6d91968cb4e05f13c0b157296ccb304389591f8
Streen9 <gkvp70@gmail.com>
2024-05-21 10:45:54 +0530

    sh file moved to terrafrom

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ module "cloudfront" {
   version = "~> 3.2.0"
 
   origin = [{
-    domain_name = data.external.bucket_exists.result["exists"] == "true" ? data.aws_s3_bucket.existing[0].bucket_regional_domain_name : aws_s3_bucket.this[0].bucket_regional_domain_name
+    domain_name           = data.external.bucket_exists.result["exists"] == "true" ? data.aws_s3_bucket.existing[0].bucket_regional_domain_name : aws_s3_bucket.this[0].bucket_regional_domain_name
     origin_id             = var.bucket_name
     origin_access_control = "s3"
   }]
